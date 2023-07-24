131a3b69f09322f383e0b11ed8c35b7b93de5fb1
Diego Garcia Saltori <dgarcia.saltori@me.com>
2023-07-24 14:51:50 -0300

    20230724-2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -27,5 +27,51 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     cy.get('.error').should('be.visible')
   })
+  it('campo telefone continua vazio quando preenchido com valor não-numerico', function(){
+    cy.get('#phone')
+      .type('abcdefghij')
+      .should('have.value', '')
+  })
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+    cy.get('#firstName').type('Diego')
+    cy.get('#lastName').type('Saltori')
+    cy.get('#email').type('diego@exemplo.com')
+    cy.get('#phone-checkbox').click()
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+    cy.get('#firstName')
+      .type('Diego')
+      .should('have.value', 'Diego')
+      .clear()
+      .should('have.value', '')
+    cy.get('#lastName')
+      .type('Saltori')
+      .should('have.value', 'Saltori')
+      .clear()
+      .should('have.value', '')
+    cy.get('#email')
+      .type('diego@exemplo.com')
+      .should('have.value', 'diego@exemplo.com')
+      .clear()
+      .should('have.value', '')
+    cy.get('#phone')
+      .type('0123456')
+      .should('have.value', '0123456')
+      .clear()
+      .should('have.value', '')
+  })
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+  it.only('envia o formuário com sucesso usando um comando customizado', function() {
+    cy.fillMandatoryFieldsAndSubmit()
+    cy.get('.success').should('be.visible')
+  })
 })
 
